1dd642918dfeb931960d51b6d7d64a22efd51baa
Brian Allen <brianallen2401@gmail.com>
2020-03-13 10:43:44 -0600

    updated sql to change price column to decimal. added insert of dummy items for db

--- bamazon.sql
+++ bamazon.sql
@@ -5,9 +5,22 @@ CREATE DATABASE bamazon;
 USE bamazon;
 
 CREATE TABLE products (
-    item_id PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    item_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(50),
     department_name VARCHAR(50),
-    price INT,
+    price DECIMAL,
     stock_quantity INT   
 )
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES
+("Amethyst Polyhedral Dice", "Dice", 7.00, 9),
+("Fire touched Polyhedral Dice", "Dice", 6.00, 5),
+("The Mighty Tarrasque Miniature", "Miniatures", 50.00, 3),
+("The Mushroom Forest Flippable Map Tiles", "Maps", 12.00, 4),
+("Tales from the Yawning Portal", "Books", 30.00, 6),
+("Explorers Guide to Wildemount", "Books", 30.00, 15),
+("The Adventurer's Campsite", "Miniatures", 35.00, 2),
+("The Sword Coase Adventurer's Guide", "Books", 25.00, 3),
+("Ancient Red Dragon Miniature", "Miniatures", 95.00, 1),
+("Purple-heart Polyhedral Dice", "Dice", 9.00, 7);
