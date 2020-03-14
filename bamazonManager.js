455e931e96589a8abf08b94e8d6bade2b5a970a9
Brian Allen <brianallen2401@gmail.com>
2020-03-13 21:09:50 -0600

    added connect to database as well as welcome message and the choose action prompt

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,12 +1,51 @@
-const inquirer = require('inquirer');
-const mysql = require('mysql');
-const colors = require('colors');
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const colors = require("colors");
 
 const connection = mysql.createConnection({
-    host: 'loclhost',
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "bamazon"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "bamazon"
 });
 
+connection.connect(function(error) {
+  if (error) throw error;
+  console.log(colors.yellow("Connected as id " + connection.threadId));
+  runBamazonManager();
+});
+
+function runBamazonManager() {
+  console.log(colors.brightGreen(`
+Welcome to Bamazon Manager View.
+
+
+`));  
+  inquirer.prompt([
+      {
+        type: "list",
+        message: "Please choose which action you would like to perform:",
+        choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product', 'Exit'],
+        name: "action"
+      }
+    ]).then(function(resp) {
+        switch (resp.action) {
+            case 'View Products for Sale':
+                viewProducts();
+                break;
+            case 'View Low Inventory':
+                viewLowInventory();
+                break;
+            case 'Add to Inventory':
+                addToInventory();
+                break;
+            case 'Add New Product':
+                addNewProduct();
+                break;
+            case 'Exit':
+                console.log(colors.bold.brightYellow("Thanks! See you next time!"));
+                connection.end();
+        }
+    });
+}
