debf01567dcfffb8a09618577ca6190ee89798ed
Brian Allen <brianallen2401@gmail.com>
2020-03-15 12:48:51 -0600

    commented out functions. added console logs to test that switch case and iquirer were working correctly.

--- bamazonManager.js
+++ bamazonManager.js
@@ -17,33 +17,51 @@ connection.connect(function(error) {
 });
 
 function runBamazonManager() {
-  console.log(colors.brightGreen(`
+  console.log(
+    colors.brightGreen(`
 Welcome to Bamazon Manager View.
-`));  
-  inquirer.prompt([
+`)
+  );
+  inquirer
+    .prompt([
       {
         type: "list",
         message: "Please choose which action you would like to perform:",
-        choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product', 'Exit'],
+        choices: [
+          "View Products for Sale",
+          "View Low Inventory",
+          "Add to Inventory",
+          "Add New Product",
+          "Exit"
+        ],
         name: "action"
       }
-    ]).then(function(resp) {
-        switch (resp.action) {
-            case 'View Products for Sale':
-                viewProducts();
-                break;
-            case 'View Low Inventory':
-                viewLowInventory();
-                break;
-            case 'Add to Inventory':
-                addToInventory();
-                break;
-            case 'Add New Product':
-                addNewProduct();
-                break;
-            case 'Exit':
-                console.log(colors.bold.brightYellow("Thanks! See you next time!"));
-                connection.end();
-        }
+    ])
+    .then(function(resp) {
+      switch (resp.action) {
+        case "View Products for Sale":
+          // viewProducts();
+          console.log("View Products selected.");
+          break;
+        case "View Low Inventory":
+          // viewLowInventory();
+          console.log("View Low Inventory selected");
+          break;
+        case "Add to Inventory":
+          // addToInventory();
+          console.log("Add to Inventory Selected");
+          break;
+        case "Add New Product":
+          // addNewProduct();
+          console.log("Add new Product selected");
+          break;
+        case "Exit":
+          console.log(colors.bold.brightYellow("Thanks! See you next time!"));
+          connection.end();
+      }
     });
+};
+
+function viewProducts() {
+
 }
