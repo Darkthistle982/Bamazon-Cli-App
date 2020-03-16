0325d34e76d5ddf21d9b9fb574927b822e500e17
Brian Allen <brianallen2401@gmail.com>
2020-03-16 14:20:59 -0600

    added database connection

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -2,3 +2,11 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 const colors = require("colors");
 const Table = require("cli-table");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "bamazon"
+  });
