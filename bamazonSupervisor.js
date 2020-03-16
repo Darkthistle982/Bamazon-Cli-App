c65fcdaec082e60e02a4dcf9633682b87425607d
Brian Allen <brianallen2401@gmail.com>
2020-03-16 14:33:49 -0600

    added the run function that will allow the user to choose what they want to do, and the switch case to trigger the chosen function

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -4,9 +4,46 @@ const colors = require("colors");
 const Table = require("cli-table");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "bamazon"
-  });
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "bamazon"
+});
+
+connection.connect(function(error) {
+  if (error) throw error;
+  console.log(colors.yellow("Connected as id " + connection.threadId));
+  runBamazonSupervisor();
+});
+
+function runBamazonSupervisor() {
+  console.log(colors.brightGreen("Welcome to bamazon Supervisor view!"));
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Please choose which action you would like to perform.",
+        choices: [
+          "View Product Sales by Department",
+          "Create New Department",
+          "Exit"
+        ],
+        name: "action"
+      }
+    ])
+    .then(function(resp) {
+      switch (resp.action) {
+        case "View Product Sales by Department":
+          viewProductSales();
+          break;
+        case "Create New Department":
+          createNewDepartment();
+          break;
+        case "Exit":
+          console.log(colors.bold.brightYellow("Thanks! See you next time!"));
+          connection.end();
+      }
+    });
+}
+
