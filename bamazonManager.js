9431b888951acb989b10ab38e41d3c6ff31a12ab
Brian Allen <brianallen2401@gmail.com>
2020-03-17 12:06:49 -0600

    added .env commands so the server login is not published.

--- bamazonManager.js
+++ bamazonManager.js
@@ -2,13 +2,14 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 const colors = require("colors");
 const Table = require("cli-table");
+const dotenv = require("dotenv").config();
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "bamazon"
+let connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE
 });
 
 connection.connect(function(error) {
